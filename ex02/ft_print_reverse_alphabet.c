aed6ea0812db306734d29bfbaa760cf0969b7210
Folarin Ogungbemi <Folarine1@gmail.com>
2022-10-26 19:27:02 +0000

    Function prints alphabets inversely

---
+++ ex02/ft_print_reverse_alphabet.c
@@ -0,0 +1,26 @@
+#include <unistd.h>
+
+void ft_putchar(char c)
+{
+    write(1, &c, 1);
+}
+
+void ft_print_alphabet(void)
+{
+    char x;
+    char y;
+
+    x = 'a';
+    y = 'z';
+    
+    while (y >= x){
+        ft_putchar(y);
+        y--;
+    }
+}
+
+int main(){
+    ft_print_alphabet();
+    ft_putchar('\n');
+    return(0);
+}
