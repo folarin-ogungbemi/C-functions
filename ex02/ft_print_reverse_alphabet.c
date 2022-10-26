0266888db7795893541ecc8e8b0a0db4b61a02a0
Folarin Ogungbemi <Folarine1@gmail.com>
2022-10-26 19:36:08 +0000

    Correct function name

--- ex02/ft_print_reverse_alphabet.c
+++ ex02/ft_print_reverse_alphabet.c
@@ -5,7 +5,7 @@ void ft_putchar(char c)
     write(1, &c, 1);
 }
 
-void ft_print_alphabet(void)
+void ft_print_reverse_alphabet(void)
 {
     char x;
     char y;
@@ -20,7 +20,7 @@ void ft_print_alphabet(void)
 }
 
 int main(){
-    ft_print_alphabet();
+    ft_print_reverse_alphabet();
     ft_putchar('\n');
     return(0);
 }
